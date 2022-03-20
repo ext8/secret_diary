85adae3dc49111e85f9e589b3803bead8612ac7b
ext8 <99179850+ext8@users.noreply.github.com>
2022-03-21 05:01:13 +0530

    basic structure

---
+++ app/main.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/env python3
+"""
+Secret Diary - the most loyal person you know is you.
+"""
+
+__author__ = "ext8"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+import pyminizip # (file,None,zip_file,password,compression_lvl)
+import zipfile
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
