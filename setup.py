d8e3b6366e21d33e27fcdbba4d8fe1fecadbc3f9
ext8 <99179850+ext8@users.noreply.github.com>
2022-03-25 22:54:15 +0530

    basic init command , util not complete

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version="0.1.0",
     author="ext8",
     license="MIT",
-    install_requires=["Click"],
+    install_requires=["click"],
     packages=find_packages(),
     entry_points={"console_scripts": ["secd=app.main:main"]},
+    url="https://github.com/ext8/secret_diary/",
+    platforms="windows"
 )
