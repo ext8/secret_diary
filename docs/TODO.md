458e6d11727b62a463db22f6ff980e860c0cb7dc
ext8 <99179850+ext8@users.noreply.github.com>
2022-03-26 22:42:26 +0530

    readme/docs writting

--- docs/TODO.md
+++ docs/TODO.md
@@ -17,6 +17,4 @@
 - meta data
 - stats
 - diary log
-
-[design]
 ```
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="secret_diary",
-    version="0.1.0",
+    version="0.2.0",
     author="ext8",
     license="MIT",
     install_requires=["click", "pyminizip"],
