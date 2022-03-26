4ad7c102823f9a3e50e2faca147d75e1559c4f52
ext8 <99179850+ext8@users.noreply.github.com>
2022-03-26 21:06:14 +0530

    setting up automatic linting

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name="secret_diary",
     version="0.1.0",
     author="ext8",
     license="MIT",
-    install_requires=["click"],
+    install_requires=["click", "pyminizip"],
     packages=find_packages(),
     entry_points={"console_scripts": ["secd=app.main:main"]},
     url="https://github.com/ext8/secret_diary/",
-    platforms="windows"
+    platforms="windows",
 )
