232c65d51d290235c48a89b086c815c5358bfa2d
ext8 <99179850+ext8@users.noreply.github.com>
2022-03-26 20:52:47 +0530

    setting up sqlite3 v2

--- app/commands/init.py
+++ app/commands/init.py
@@ -1,11 +1,16 @@
-import click
+"""sced init :: command"""
+
 import time
+
+import click
 from utils import NewDiary, SQLog
 
 diary = NewDiary()
 
 
 class Context:
+    """sced init -d DIRECTORY :: context"""
+
     def __init__(self, directory) -> None:
         self.directory = directory
 
@@ -23,20 +28,20 @@ def main(ctx, directory: str) -> None:
     """create or reinitialize sec.d repo"""
     ctx.object = Context(directory)
 
-    CONFIG_DIR, CONFIG_SUBDIR = diary.make(directory=directory)
+    config_dir, config_subdir = diary.make(directory=directory)
 
-    UNIX_TIME_FILE = f"{CONFIG_DIR}//{CONFIG_SUBDIR[0]}//time-stamp.db"
+    unix_time_file = f"{config_dir}//{config_subdir[0]}//time-stamp.db"
 
-    UNIX_TIME = int(time.time())
+    unix_time = int(time.time())
 
-    """
-    timestamps for opening / closing diary
+    with SQLog(unix_time_file) as cur:
 
-    sqlite> | SrNo 	| LogType 	| UnixTime 	|
-            |------	|--------	|----------	|
-            | 1.   	| Open   	| 83451751 	|
-    """
-    with SQLog(UNIX_TIME_FILE) as c:
+        """timestamps for opening / closing diary
 
-        c.execute("CREATE TABLE time_stamp ( SrNo int,LogType text,UnixTime int)")
-        c.execute("INSERT INTO time_stamp VALUES (?, ?)", (1, "Open", UNIX_TIME))
+        sqlite> | SrNo 	| LogType 	| UnixTime 	|
+                |------	|--------	|----------	|
+                | 1.   	| Open   	| 83451751 	|
+        """
+
+        cur.execute("CREATE TABLE time_stamp ( SrNo int,LogType text,UnixTime int)")
+        cur.execute("INSERT INTO time_stamp VALUES (?, ?)", (1, "Open", unix_time))
